7014d2046a57375097569d6e74c9f33c8dfd952b
Dannick Pomerleau <dannickp@hotmail.com>
2021-02-27 13:59:57 -0500

    Fixed build script

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ podman create --name shammancer-git-io --replace -p 8080:80 localhost/shammancer
 rm -r build/
 podman cp shammancer-git-io:/usr/local/apache2/htdocs/ build
 podman rm shammancer-git-io
+mv htdocs build
